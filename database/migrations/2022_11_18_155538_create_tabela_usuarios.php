91b47cf70b999d4065f511e76a0f939d5d414cd5
karin morais <andradkarin@gmail.com>
2022-11-18 13:01:34 -0300

    Criação da migration de usuarios

---
+++ database/migrations/2022_11_18_155538_create_tabela_usuarios.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->string('telefone');
+            $table->string('email')->unique();
+            $table->string('cep');
+            $table->string('estado');
+            $table->string('cidade');
+            $table->string('bairro');
+            $table->string('endereco');
+            $table->string('endereco_numero');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
