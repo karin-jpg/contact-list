ac87e037c3cf5610489cb6f09bd4897866619bef
karin morais <andradkarin@gmail.com>
2022-11-18 16:00:50 -0300

    Criada classe de testes

---
+++ tests/Feature/UsuariosControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace tests\Controller;
+
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use Illuminate\Http\Response;
+use Tests\TestCase;
+use Illuminate\Support\Facades\DB;
+
+
+class UsuariosControllerTests extends TestCase
+{
+
+}
