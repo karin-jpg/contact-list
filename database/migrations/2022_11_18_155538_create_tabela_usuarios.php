47ebf25e075daa49f25a2af8926c2d227a8b5ba1
karin morais <andradkarin@gmail.com>
2022-11-19 12:24:12 -0300

    Removed unique from migration

--- database/migrations/2022_11_18_155538_create_tabela_usuarios.php
+++ database/migrations/2022_11_18_155538_create_tabela_usuarios.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('nome');
             $table->string('telefone');
-            $table->string('email')->unique();
+            $table->string('email');
             $table->string('cep');
             $table->string('estado');
             $table->string('cidade');
