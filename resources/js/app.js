6a2795e7902d3c547cd62c90d50bf6cbdd698916
karin morais <andradkarin@gmail.com>
2022-11-19 17:42:48 -0300

    Configurado página inicial de vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
+
 import './bootstrap';
+
+import { createApp, createElementBlock } from 'vue'
+
+import app from './components/app.vue'
+
+createApp(app).mount('#app')
---
+++ resources/js/components/app.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>salve</h1>
+</template>
