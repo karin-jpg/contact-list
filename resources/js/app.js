7a4af05bb84f95c05a3acfe32135625d595ebcaa
karin morais <andradkarin@gmail.com>
2022-11-20 10:12:27 -0300

    Configuração das rotas do vue router

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,18 +5,22 @@ import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue'
-import Agenda from './components/agenda.vue'
-import AgendaForm from './components/agenda-form.vue'
+import Agenda from './views/Agenda.vue'
+import AgendaFormulario from './components/Agenda-formulario.vue'
+import CriarContato from './views/CriarContato.vue'
+import EditarContato from './views/EditarContato.vue'
 
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', component: Agenda },
-        { path: '/form', component: AgendaForm }
+        { path: '/criar-contato', component: CriarContato},
+        { path: '/editar', component: EditarContato }
     ]
 });
 
 createApp(App)
     .use(router)
+    .component('agenda-formulario', AgendaFormulario)
     .mount('#app')
